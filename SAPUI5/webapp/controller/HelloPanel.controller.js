2e5d52653d0023efbd6f65187a20593cf2275c59
wpoclava <wpoclava@gmail.com>
2021-04-10 23:56:16 +0000

    15 Herramientas depuracion

--- SAPUI5/webapp/controller/HelloPanel.controller.js
+++ SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,19 +1,31 @@
 // @ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.base.Log} Log
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict";
 
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
 
             onInit: function () {
 
+            },
+
+            onBeforeRendering: function () {
+                window.message = 'Log message = onBeforeRendering';
+                Log.info(window.message);
+                Log.error(window.message);
+            },
+
+            onAfterRendering: function () {
+                //debugger;
             },
 
             onShowHello: function () {
